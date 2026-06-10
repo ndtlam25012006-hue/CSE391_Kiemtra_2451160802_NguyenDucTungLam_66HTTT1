fd9da2b8e88f20f80ba9c67a437510f04abd5565
Nguyen Duc Tung Lam <ndtlam25012006@gmail.com>
2026-06-10 16:32:57 +0700

    Them du lieu mau

---
+++ data.js
@@ -0,0 +1,38 @@
+const records = [
+
+    {
+        school: "Trường THPT Nguyễn Huệ",
+        department: "Phòng GDĐT TP. Thủy Lợi",
+        status: "Hoàn thành",
+        description: "Đã hoàn thành chuyển đổi số toàn trường."
+    },
+    
+    {
+        school: "Trường THCS Lê Quý Đôn",
+        department: "Phòng GDĐT H. Thủy Nguyên",
+        status: "Đang triển khai",
+        description: "Đang triển khai học bạ điện tử."
+    },
+    
+    {
+        school: "Trường THPT Trần Phú",
+        department: "Phòng GDĐT Q. Hồng Bàng",
+        status: "Chưa bắt đầu",
+        description: "Đang chờ kế hoạch thực hiện."
+    },
+    
+    {
+        school: "Trường THCS Nguyễn Tri Phương",
+        department: "Phòng GDĐT TP. Thủy Lợi",
+        status: "Đang triển khai",
+        description: "Đã hoàn thành khoảng 60%."
+    },
+    
+    {
+        school: "Trường Tiểu học Kim Đồng",
+        department: "Phòng GDĐT Q. Ngô Quyền",
+        status: "Hoàn thành",
+        description: "Đã triển khai website và sổ liên lạc điện tử."
+    }
+    
+    ];
